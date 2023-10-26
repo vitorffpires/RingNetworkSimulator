7adcf799a88a43a5bbcb438fd7b6c58c520feb02
Thiago Macedo <thialmaceod@gmail.com>
2023-10-26 09:25:57 -0300

    NetworkNode init method

--- src/network/network_node.py
+++ src/network/network_node.py
@@ -1,6 +1,18 @@
 from typing import Optional
+from .message_queue import MessageQueue
 
 class NetworkNode:
     
-    def __init__(self) -> None:
-        pass
+    def __init__(self,
+                 IP: str,
+                 nickname: str,
+                 right_neighbor: Optional[str],
+                 has_token: bool = False) -> None:
+        
+        self.IP = IP
+        self.nickname = nickname
+        self.right_neighbor = right_neighbor
+        self.has_token = has_token
+        self.message_queue = MessageQueue()
+        
+    
